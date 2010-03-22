4b9f48405c636013ed1b92a8866c8c2b0ae24d97
Kamil Baldyga <kbaldyga@gmail.com>
2010-03-22 23:19:13 +0100

    ladniejsze zaprzyjaznione. tamte nie byly przyjazne. w ogole

--- zweite/zad1.erl
+++ zweite/zad1.erl
@@ -1,5 +1,5 @@
 -module(zad1).
--export([zaprzyjaznione/1]).
+-export([zaprzyjaznione/1,dzielniki/1, zaprzyjaznione/2]).
 
 % generowanie listy dzielnikow liczy
 dzielniki(Liczba) when is_integer(Liczba) ->
@@ -14,42 +14,10 @@ dzielniki(Liczba,Aktualny,Acc,Guard) when Aktualny =< Guard ->
 dzielniki(Liczba,_,Acc,_) ->
     lists:reverse(Acc) ++ lists:map(fun (X) -> round(Liczba/X) end,Acc).
 
-% suma elementow na liscie
-sum([])->
-    0;
-sum([H|T]) ->
-    H+sum(T).
 
-% lista bez ostatniego elementu
-init([]) ->
-    [];
-init(List) ->
-    [_|T] = lists:reverse(List),
-    lists:reverse(T).
-
-% wygenerowanie wszystkich par {liczba,suma dzielnikow liczby}
-sumaDzielnikow(Liczba) ->
-    lists:map(fun(X) ->
-                      {X,sum(init(dzielniki(X)))} end,lists:seq(1,Liczba)).
-
-% generator liczb zaprzyjaznionych
-zaprzyjaznioneprim(N) ->
-    [ {X,Y} || {X,Y} <- sumaDzielnikow(N),
-               {A,B} <- sumaDzielnikow(N),A==Y,X==B,A=/=X].
-
-% usuwaczka jednej z par ( [{A,B},{B,A}] -> [{A,B}] )
-usunDuplikat(List) ->
-    usunDuplikat(List,[]).
-usunDuplikat([],Acc) ->
-    Acc;
-usunDuplikat([{A,B}|T],Acc) ->
-    case lists:member({B,A},Acc) of true ->
-            usunDuplikat(T,Acc);
-        false ->
-            usunDuplikat(T,[{A,B}|Acc])
-    end.
-
-% ostateczna funkcja
+zaprzyjaznione(X,Y) ->
+    (lists:sum(dzielniki(X))-X == Y) and (lists:sum(dzielniki(Y))-Y == X).
 zaprzyjaznione(N) ->
-    X = zaprzyjaznioneprim(N),
-    lists:reverse(usunDuplikat(X)).
+    [ {X,Y} || X <- lists:seq(1,N),
+               Y <- lists:seq(X+1,N),
+               zaprzyjaznione(X,Y) ].
